bf96a209d5b35784b4fd15e52079e319683ec24f
Hondamir Kobilov <hondamirkobilov02@gmail.com@example.com>
2025-07-30 11:25:46 +0500

    main photo o'zgardi

--- Bot/config.py
+++ Bot/config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "8043108480:AAGZoYnznJlett0tDz-ATqZMwNJMXjT4BWU" # Bot tokeni
+BOT_TOKEN = "7676105696:AAF_q9C6dZd6CvNOzvx3g4dAKNdr6VAD2Mw" # Bot tokeni
 API_BASE_URL = "https://admin1.ndc-agency.uz/api"
 # APIni shu joyga yozasiz Etibor bering linkni yozib oxirida /api yozish shart
 #NGROK_URL = "https://c438-2a05-45c2-61c7-a800-d8fa-9ece-6d15-b8c2.ngrok-free.app"  #Teginmaysiz
